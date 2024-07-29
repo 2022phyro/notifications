2e79cb82615f5413de1389bd245ffa2037a219ee
2022phyro <phyrokelstein2@gmail.com>
2024-07-29 14:27:34 +0100

    Reset the logs to be only command line

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -4,16 +4,6 @@ const expressTransport = pino.transport({
   targets: [
     {
       level: 'trace',
-      target: 'pino/file',
-      options: {
-        destination: 'logs/express.log',
-        translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - - {responseTime} ms',
-        hideObject: false
-      }
-    },
-    {
-      level: 'trace',
       target: 'pino-pretty',
       options: {
         destination: 1,
@@ -27,15 +17,6 @@ const expressTransport = pino.transport({
 
 const generalTransport = pino.transport({
   targets: [
-    {
-      level: 'trace',
-      target: 'pino/file',
-      options: {
-        destination: 'logs/general.log',
-        translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-        hideObject: false
-      }
-    },
     {
       level: 'trace',
       target: 'pino-pretty',
